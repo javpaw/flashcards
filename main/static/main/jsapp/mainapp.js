0f98f4293a1d3769f4d78416582efaa76affdfd9
jav <jav@jav-big.(none)>
2012-09-24 14:31:17 -0500

    added some files, and some templates created

---
+++ main/static/main/jsapp/mainapp.js
@@ -0,0 +1,21 @@
+'use strict';
+
+
+var fcApp = angular.module('fcApp',['fcApp.directives','fcApp.services']).// ['fcApp.filters', 'fcApp.services', 'fcApp.directives','ngResource']).
+  config([
+   '$interpolateProvider',function($interpolateProvider){
+    $interpolateProvider.startSymbol('<{');
+    $interpolateProvider.endSymbol('}>');
+  }
+]);
+
+
+// fcApp.config([
+//   '$routeProvider', function($routeProvider) { 
+//      $routeProvider.when("/manual", {templateUrl:"/static/fc/partials/manual.html", controller:"fcController"});
+//      $routeProvider.when("/auto", {templateUrl:"/static/fc/partials/auto.html", controller:"autoController"});  
+//      $routeProvider.when("/results", {templateUrl:"/static/fc/partials/results.html", controller:"resultController"});  
+//   	 $routeProvider.otherwise({redirectTo:"/auto"});
+// 	}
+// ]);
+
