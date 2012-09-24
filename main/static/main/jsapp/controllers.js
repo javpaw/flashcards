d07592381387c2b417507fc12775457ec98ca98c
javier <thisisnotmy@ema.il>
2012-09-24 18:07:10 -0500

    added watcher of arrows keys to trainerController

--- main/static/main/jsapp/controllers.js
+++ main/static/main/jsapp/controllers.js
@@ -5,6 +5,30 @@ function trainerController($scope){
     {title:'fc2', desc:"flashcard number 2"},
     {title:'fc3', desc:"flashcard number 3"}
   ];
-  $scope.actualFc = $scope.flashCards[0];
-  
+  $scope.actualFcIndex = 0
+  $scope.actualFc = $scope.flashCards[$scope.actualFcIndex];
+
+  $scope.$watch('key_changed',function(){
+    switch($scope.key){
+      case 37:
+      console.log("left");
+      break;
+
+      case 38:
+      console.log("up");
+      break;
+
+      case 39:
+      console.log("right");
+      $scope.actualFcIndex++;
+      $scope.actualFc = $scope.flashCards[$scope.actualFcIndex];
+      break;
+      
+      case 40:
+      break;
+      
+
+    }
+  });
 }
+trainerController.$inject=["$scope"]//,"$location","$timeout" ,"fcService"]
--- main/static/main/jsapp/directives.js
+++ main/static/main/jsapp/directives.js
@@ -4,15 +4,13 @@ angular.module('fcApp.directives', []).
   		scope.key_changed = 0;
   		$(document).keydown(function(e){
         var arrows = [37,38,39,40];
-        console.log(  _.include(arrows,e.which))
-
         if (_.include(arrows,e.which)){ 
-         scope.key_changed++;
+         scope.key_changed=scope.key_changed+1;
          scope.key = e.which;
-         console.log("key pressed",scope.key);
-         e.preventDefault();  
+         e.preventDefault();
+         scope.$apply();
         } 
-         
+
     	});
   	}
   }]);
