97741b960c9db34460326594b1a066679be0535f
jav <jav@jav-big.(none)>
2012-09-24 11:04:33 -0500

    added js libraries and some templates directories

--- flashcards/urls.py
+++ flashcards/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^$', TemplateView.as_view(template_name="main/index.html"))
 )
