2ac67d6ff249e4430514f2bd1835620335dffa2a
jav <jav@jav-big.(none)>
2012-09-24 15:45:43 -0500

    added some stub flash cards to explore the domain problem

--- main/static/main/jsapp/controllers.js
+++ main/static/main/jsapp/controllers.js
@@ -1,3 +1,10 @@
 function trainerController($scope){
-	$scope.test = "testController";
+
+  $scope.flashCards = [
+    {title:'fc1', desc:"flashcard number 1"},
+    {title:'fc2', desc:"flashcard number 2"},
+    {title:'fc3', desc:"flashcard number 3"}
+  ];
+  
+  $scope.actualFc = $scope.flashCards[0];
 }
