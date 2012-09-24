0847f6cd1c53cf25d7d2f2b95147b1ee85ef7a45
jav <jav@jav-big.(none)>
2012-09-24 15:10:10 -0500

    testing some drafts for the page

--- flashcards/urls.py
+++ flashcards/urls.py
@@ -7,5 +7,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
 
-    url(r'^$', TemplateView.as_view(template_name="main/index.html"))
+    url(r'^$', TemplateView.as_view(template_name="main/index.html")),
+    url(r'^training$', TemplateView.as_view(template_name="main/training.html")),
+
 )
