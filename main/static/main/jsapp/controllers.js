72548d70449ca5ba97c8befe65c33ecb58a2a098
javier <thisisnotmy@ema.il>
2012-09-24 19:57:55 -0500

    added some comments to trainerController

--- main/static/main/jsapp/controllers.js
+++ main/static/main/jsapp/controllers.js
@@ -1,27 +1,28 @@
 function trainerController($scope){
-
+  /*
+    Main controller for a training session with a bundle of flashcards.
+  */
+ 
   $scope.flashCards = [
     {title:'fc1', desc:"flashcard number 1",level:0},
     {title:'fc2', desc:"flashcard number 2",level:0},
     {title:'fc3', desc:"flashcard number 3",level:0}
   ];
 
-  var startFc = {title:'Start', desc:"right arrow to start",level:0};
-  var finishFc = {title:'Finish', desc:"Congratulations!!! You completed your training",level:0};  
+  var startFc = {title:'Start', desc:"right arrow to start",level:0};//Special flashcard for initial instructions
+  var finishFc = {title:'Finish', desc:"Congratulations!!! You completed your training",level:0};//special flashcard for a finished session  
 
-
-  // $scope.minLevel = 0;
-  // $scope.currentFcs = [];
-  // $scope.currentIndex = 0;
-  var maxLimit = 3;
-  var firstTime = true;
-  var state = 'active';
-  var minLevel = 0;
+  var maxLimit = 3; //Default numbers of levels to practice starting at 0
+  var firstTime = true; //flag to check initialization of the training session
+  var state = 'active'; //state of the training session the other option is finished
+  var minLevel = 0;//initial level for all the fc cards
 
   var nextLevel = function(){
+    /*
+    At the end of one level check if the training is finished and get the cards for the next level
+    */
     minLevel = _.min($scope.flashCards,function(item){return item.level;}).level;
-    console.log(minLevel);
-    if ($scope.minLevel >= maxLimit){
+    if (minLevel >= maxLimit){
       state = "finished";
     }
     else{
@@ -31,13 +32,20 @@ function trainerController($scope){
   } //end next_level
 
 
-  $scope.currentFc = startFc
+  $scope.currentFc = startFc;
   
   $scope.$watch('key_changed',function(){
+    /*
+    When a key arrow is pressed take action.
+    The listener for the keys is defined fcApp.directives
+    */
+
     if(state==="finished"){
+      //This session ended.
       $scope.currentFc = finishFc
       return;
     }
+
     switch($scope.key){
       case 37:
       console.log("left");
@@ -63,12 +71,7 @@ function trainerController($scope){
       
       case 40:
       break;
-      
-
     }
   });
-
-
-
 }
 trainerController.$inject=["$scope"]//,"$location","$timeout" ,"fcService"]
